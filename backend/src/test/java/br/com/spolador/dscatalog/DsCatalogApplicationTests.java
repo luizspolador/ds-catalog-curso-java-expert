e1846533a251ae0c42065c3efc63ea76ec5c84c7
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-12-07 16:17:46 -0300

    projeto criado

---
+++ backend/src/test/java/br/com/spolador/dscatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spolador.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
